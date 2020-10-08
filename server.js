392ed8bde930b94be8825e6d4cfa85d660af3825
tavolafourcade <tavolafourcade@gmail.com>
2020-10-08 13:55:31 -0500

    Make transaction feature for both Register and Login

--- server.js
+++ server.js
@@ -78,12 +78,38 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
     const {email, name, password} = req.body;
 
+    
     //Password encryption
     // bcrypt.hash(password, null, null, function(err, hash) {
     //     // Store hash in your password DB.
     //     console.log(hash);
     // });
 
+    //Implementing the password encryption with DB
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx =>{
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail =>{
+            //Using Knex\
+            return trx('users')
+            .returning('*') //users insert raul and return all columns
+            .insert({
+                email: loginEmail[0],
+                name: name,
+                joined: new Date()
+            })
+            .then(user => {
+            res.json(user[0]); 
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
     //Now we're going to use knex instead of the local DB
     // database.users.push(
     //     {
@@ -96,17 +122,7 @@ app.post('/register', (req, res) => {
     //     }
     // )
 
-    //Using Knex\
-    db('users')
-        .returning('*')
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date()
-    })
-    .then(user => {
-        res.json(user[0]); 
-    })
+ 
     .catch(err => res.status(400).json('unable to register'));
 
 })
@@ -114,35 +130,58 @@ app.post('/register', (req, res) => {
 // Profile
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id){
-            found = true;
-            return res.json(user);
+    //let found = false;
+    //Using knex
+    // database.users.forEach(user => {
+    //     if (user.id === id){
+    //         found = true;
+    //         return res.json(user);
+    //     }
+    // })
+    db.select('*').from('users').where({
+        id: id
+    }).then(user => {
+        if (user.length){
+            res.json(user[0]);
         }
+        
     })
+    .catch(err => res.status(400).json('Not found'))
 
-    if (!found){
-        res.status(400).json('not found')
-    }
+    // if (!found){
+    //     res.status(400).json('not found')
+    // }
 })
 
+//The image endpoint updates the entries and it increases the count
 app.put('/image',(req,res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
+    
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
     })
-
-    if (!found){
-        res.status(400).json('not found there')
-    }
+    .catch(err => res.status(400).json('Unable to get entries'))
 
 })
+
+    //Since were using Knex, we don't need this anymore
+    // let found = false;
+    // database.users.forEach(user => {
+    //     if (user.id === id) {
+    //         found = true;
+    //         user.entries++;
+    //         return res.json(user.entries);
+    //     }
+    // })
+
+    // if (!found){
+    //     res.status(400).json('not found there')
+    // }
+
+
 
 //We're using the port 3000 to listen
 app.listen(3000, () => {
